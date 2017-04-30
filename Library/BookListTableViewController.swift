2f7003eb5bfb6908f4dcc39af6e66728f617597e
柳本宗達 <juventus.y.1104@gmail.com>
2017-05-01 01:40:04 +0900

    再構築

---
+++ Library/BookListTableViewController.swift
@@ -0,0 +1,45 @@
+//
+//  BookListTableViewController.swift
+//  Library
+//
+//  Created by 柳本宗達 on 2017/05/01.
+//  Copyright © 2017年 柳本宗達. All rights reserved.
+//
+
+import UIKit
+
+class BookListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+    
+    @IBAction func backToBookList(segue: UIStoryboardSegue){
+        
+    }
+
+
+  }
---
+++ Library/BookTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BookTableViewCell.swift
+//  Library
+//
+//  Created by 柳本宗達 on 2017/05/01.
+//  Copyright © 2017年 柳本宗達. All rights reserved.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
